0ef33aecae6cb1f0fa6632a324351016b86a0b61
Aljon Layson <aljonlayson21@gmail.com>
2023-06-09 06:49:24 +0800

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from '@components/Feed'
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover and Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">This is an open source prompt sharing website developed using next.js.</p>
+        
+        <Feed/>
+    </section>
+  )
+}
+
+export default Home
